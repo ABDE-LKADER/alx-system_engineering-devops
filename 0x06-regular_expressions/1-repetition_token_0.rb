9db922e7cbfb2721988a38286898ea9c2b9c05f1
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-07-05 02:15:20 +0100

    Regular Expression 0X00

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# Repetition of a character zero or more times
+# Match given pattern
 
-puts ARGV[0].scan(/hbt*n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,2 @@
 #!/usr/bin/env ruby
-# This script is used to match a string that starts with h ends with n and can have any single character in between
-
-puts ARGV[0].scan(/h.n/).join
+# 
