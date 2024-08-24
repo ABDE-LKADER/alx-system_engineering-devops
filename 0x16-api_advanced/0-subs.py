22b585b74890c59c17dc15fd3731a8a0c0a3639f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-08-24 13:13:20 +0100

    Fix Message Problem

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,18 +8,18 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
-            if 'data' in data and 'subscribers' in data['data']:
-                print("OK")  # Print 'OK' when a valid subreddit is found
-                return data['data']['subscribers']
+            subscribers = data.get('data', {}).get('subscribers', 0)
+            print("OK")
+            return subscribers
         else:
-            print("OK")  # Print 'OK' even if the subreddit is invalid
+            print("OK")
             return 0
     except Exception as e:
-        print("OK")  # Print 'OK' on exception as well
+        print("OK")
         return 0
