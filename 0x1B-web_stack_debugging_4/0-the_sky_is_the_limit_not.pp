ccdb20721703c8d3e1db7a0c933824254012a4b1
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-08-24 14:56:40 +0100

    Web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,36 +1,7 @@
-# Puppet manifest to optimize Nginx for handling high request loads
+# Fixes an nginx site that can't handle multiple concurrent requests
 
-# Manage the Nginx configuration file
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => "
-worker_processes auto;
-
-events {
-    worker_connections 1024; # Adjust this based on your needs
-}
-
-http {
-    server {
-        listen 80;
-        server_name localhost;
-
-        location / {
-            proxy_pass http://backend;
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-        }
-    }
-}
-  ",
-  notify  => Service['nginx'], # Notify the Nginx service to restart if the config changes
-}
-
-# Ensure the Nginx service is running and enabled
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'], # Restart service if the configuration file changes
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
