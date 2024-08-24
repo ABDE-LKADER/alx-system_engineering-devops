d32302a7fdd51b0d24c2dcde5a970c8c1b05f3ed
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-08-24 13:16:53 +0100

    Fix beaty norm

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
-Function to count words in all hot posts of a given Reddit subreddit.
-"""
+""" Function to count words in all hot posts of a given Reddit subreddit. """
 import requests
 
 
 def count_words(subreddit, word_list, after=None, counts={}):
-    """
-    Recursive function that queries the Reddit API, parses the title of all
-        hot articles, and prints a sorted count of given keywords
-    """
+    """ Recursive function that queries the Reddit API, parses the title of all
+        hot articles, and prints a sorted count of given keywords """
     if not word_list or word_list == [] or not subreddit:
         return
 
