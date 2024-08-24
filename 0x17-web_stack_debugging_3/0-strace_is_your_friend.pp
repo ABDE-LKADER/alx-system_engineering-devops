1f668a43ba6de0d6b0c2b22938d4d3575a327452
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-08-24 12:41:06 +0100

    Fixes Bad phpp Extensions To php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Fixes Bad `phpp` Extensions To `php`
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/usr/bin/:/bin/'
 }
